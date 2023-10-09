4fec168bf1cd3a2124d9270a90e8ed8b4464fe41
Hooray Hu <304327508@qq.com>
2023-10-09 23:34:19 +0800

    archive v3 version docs

---
+++ .vitepress/theme/index.ts
@@ -0,0 +1,31 @@
+import { h, onMounted, watch, nextTick } from 'vue'
+import Theme from 'vitepress/theme'
+import { useRoute } from 'vitepress'
+import './fonts/fira_code/fira_code.css'
+import './styles/var.css'
+import mediumZoom from 'medium-zoom'
+import HomePreview from './components/HomePreview.vue'
+import ZoomImg from './components/ZoomImg.vue'
+
+export default {
+  ...Theme,
+  Layout() {
+    return h(Theme.Layout, null, {
+      'home-features-after': () => h(HomePreview),
+    })
+  },
+  setup() {
+    const route = useRoute()
+    const initZoom = () => {
+      mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' })
+    }
+    onMounted(() => initZoom()),
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom()),
+    )
+  },
+  enhanceApp({ app }) {
+    app.component('ZoomImg', ZoomImg)
+  },
+}
